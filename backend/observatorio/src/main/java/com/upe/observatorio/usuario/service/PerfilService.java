b5e218355d10a69ce806200a5c54deb811c613aa
Kvojps <josebazjunior09@gmail.com>
2022-12-13 13:21:01 -0300

    refatorando servico do usuario

--- backend/observatorio/src/main/java/com/upe/observatorio/usuario/service/PerfilService.java
+++ backend/observatorio/src/main/java/com/upe/observatorio/usuario/service/PerfilService.java
@@ -6,15 +6,29 @@ import java.util.Optional;
 import com.upe.observatorio.usuario.domain.Perfil;
 import com.upe.observatorio.usuario.domain.dto.PerfilDTO;
 
-public interface PerfilService {
-	
-	public List<Perfil> listarPerfis();
-	
-	public Optional<Perfil> buscarPerfilPorId(Long id);
-	
-	public Perfil adicionarPerfil(PerfilDTO perfil);
-	
-	public void atualizarPerfil(PerfilDTO perfil, Long id);
-	
-	public void removerPerfil(Long id);
+public class PerfilService{
+
+	public List<Perfil> listarPerfis() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Optional<Perfil> buscarPerfilPorId(Long id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	public Perfil adicionarPerfil(PerfilDTO perfil) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void atualizarPerfil(PerfilDTO perfil, Long id) {
+		// TODO Auto-generated method stub
+	}
+
+	public void removerPerfil(Long id) {
+		// TODO Auto-generated method stub
+	}
+
 }
--- backend/observatorio/src/main/java/com/upe/observatorio/usuario/service/UsuarioService.java
+++ backend/observatorio/src/main/java/com/upe/observatorio/usuario/service/UsuarioService.java
@@ -3,18 +3,35 @@ package com.upe.observatorio.usuario.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
 import com.upe.observatorio.usuario.domain.Usuario;
 import com.upe.observatorio.usuario.domain.dto.UsuarioDTO;
 
-public interface UsuarioService {
+@Service
+public class UsuarioService {
 
-	public List<Usuario> listarUsuarios();
-	
-	public Optional<Usuario> buscarUsuarioPorId(Long id);
-	
-	public Usuario adicionarUsuario(UsuarioDTO usuario);
-	
-	public void atualizarUsuario(UsuarioDTO usuario, Long id);
-	
-	public void removerUsuario(Long id);
+	public List<Usuario> listarUsuarios() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Optional<Usuario> buscarUsuarioPorId(Long id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	public Usuario adicionarUsuario(UsuarioDTO usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void atualizarUsuario(UsuarioDTO usuario, Long id) {
+		// TODO Auto-generated method stub
+	}
+
+	public void removerUsuario(Long id) {
+		// TODO Auto-generated method stub
+	}
+
 }
