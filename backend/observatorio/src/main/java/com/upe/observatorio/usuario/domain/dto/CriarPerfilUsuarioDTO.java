f093950371ed17dac82e6077269e38727122cbc9
Kvojps <josebazjunior09@gmail.com>
2023-01-13 18:05:37 -0300

    adicioando documentacao swagger ao escopo de usuario

--- backend/observatorio/src/main/java/com/upe/observatorio/usuario/domain/dto/CriarPerfilUsuarioDTO.java
+++ backend/observatorio/src/main/java/com/upe/observatorio/usuario/domain/dto/CriarPerfilUsuarioDTO.java
@@ -2,11 +2,14 @@ package com.upe.observatorio.usuario.domain.dto;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CriarPerfilUsuarioDTO {
+	@Schema(example = "1", description = "Id referente ao usuário")
 	private Long idUsuario;
 	
+	@Schema(example = "[1,2]", description = "Id referente aos perfis")
 	private List<Long> idPerfis;
 }
