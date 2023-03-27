17a1c565dce5ec0b518735ed6b927e1d2c07ed7e
José Ferreira <josebazjunior09@gmail.com>
2023-03-27 16:45:12 -0300

    adicionando inserts campus e cursos

---
+++ database/inserts/v1/insertCampusCurso.sql
@@ -0,0 +1,210 @@
+--Ciências biológicas
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (49, 5, 13);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (50, 6, 13);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (51, 9, 13);
+
+--Ciências sociais
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (52, 10, 14);
+
+--Computação
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (53, 5, 15);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (54, 6, 15);
+
+--Educação Física
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (55, 10, 16);
+
+--Geografia
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (56, 5, 17);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (57, 6, 17);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (58, 9, 17);
+
+--História
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (59, 5, 18);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (60, 6, 18);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (61, 9, 18);
+
+--Letras Portugues/Espanhol
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (62, 6, 19);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (63, 9, 19);
+
+--Letras Portugues/Inglês
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (64, 6, 20);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (65, 9, 20);
+
+--Letras Portugues/Literaturas
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (66, 5, 21);
+
+--Matemática
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (67, 5, 22);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (68, 6, 22);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (69, 9, 22);
+
+--Pedagogia
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (70, 5, 23);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (71, 6, 23);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (72, 9, 23);
+
+--Bacharelado
+
+--Administração
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (73, 2, 24);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (74, 4, 24);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (75, 8, 24);
+
+--Ciências biológicas
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (76, 10, 25);
+
+--Direito
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (77, 1, 25);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (78, 2, 25);
+
+--Educação Física
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (79, 10, 26);
+
+--Enfermagem
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (80, 9, 27);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (81, 10, 27);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (82, 107, 27);
+
+--Engenharia Civil
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (83, 2, 28);
+
+--Engenharia da computação
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (84, 2, 28);
+
+--Engenharia de controle e automação
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (85, 2, 29);
+
+--Engenharia de software
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (86, 5, 30);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (87, 12, 30);
+
+--Engenharia elétrica de teleco
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (88, 2, 31);
+
+--Engenharia elétrica eletrônica
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (89, 2, 32);
+
+--Engenharia elétrica eletrotécnica
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (90, 2, 33);
+
+--Engenharia mecânica
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (91, 2, 34);
+
+--Física de materiais
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (92, 2, 35);
+
+--Fisioterapia
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (93, 9, 36);
+
+--Medicina
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (94, 5, 37);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (95, 10, 37);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (96, 11, 37);
+
+--Nutrição
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (97, 9, 38);
+
+--Odontologia
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (98, 3, 39);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (99, 1, 39);
+
+--Psicologia
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (100, 5, 40);
+
+--Saúde coletiva
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (101, 10, 41);
+
+--Serviço social
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (102, 48, 42);
+
+--SI
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (103, 4, 43);
+
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (104, 12, 43);
+
+--Terapia ocupacional
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (105, 10, 44);
+
+--Tecnologia em logística
+INSERT INTO public.campus_curso(id, id_campus, id_curso)
+	VALUES (106, 6, 45);
