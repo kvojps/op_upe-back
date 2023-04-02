cb1b891aabd89683b7902eb1f2c70a8810a1106c
José Ferreira <josebazjunior09@gmail.com>
2023-04-02 01:31:46 -0300

    adicionando cors

--- backend/observatorio/src/main/java/com/upe/observatorio/projeto/controlador/ProjetoAPI.java
+++ backend/observatorio/src/main/java/com/upe/observatorio/projeto/controlador/ProjetoAPI.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,6 +37,7 @@ import com.upe.observatorio.utils.ObservatorioExcecao;
 
 @RestController
 @RequestMapping("api/projeto")
+@CrossOrigin
 public class ProjetoAPI {
 
 	@Autowired
