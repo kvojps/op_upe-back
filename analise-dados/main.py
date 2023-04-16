cdb3e088cab76120081a201d337d95d16ac3ec38
José Ferreira <josebazjunior09@gmail.com>
2023-04-16 20:27:58 -0300

    adicionando descricoes

--- analise-dados/main.py
+++ analise-dados/main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
+#abrir arquivo
 df = pd.read_excel('projetos.xlsx')
 
+#adicionar coluna curso
 df['2. Campus / Unidade / Curso: *'] = df['2. Campus / Unidade / Curso: *'].astype(str).str.lower()
 df['curso'] = ''
 cursos = ['Ciências Biológicas', 'Ciências Sociais', 'Computação', 'Educação Física', 'Geografia', 'História', 'Letras com habilitação em língua portuguesa e espanhola', 'Letras com habilitação em língua portuguesa e inglesa', 'Letras com habilitação em língua portuguesa e suas literaturas', 'Matemática', 'Pedagogia', 'Administração', 'Ciências biológicas', 'Direito', 'Educação Física', 'Enfermagem', 'Engenharia civil', 'Engenharia da computação', 'Engenharia de controle e automação', 'Engenharia de software', 'Engenharia elétrica de telecomunicações', 'Engenharia elétrica eletrônica', 'Engenharia elétrica eletrotécnica', 'Engenharia mecânica', 'Física de materiais', 'Fisioterapia', 'Medicina', 'Nutrição', 'Odontologia', 'Psicologia', 'Saúde coletiva', 'Serviço social', 'Sistemas de informação', 'Terapia ocupacional', 'Tecnologia em logística']
@@ -11,8 +13,8 @@ for index, row in df.iterrows():
             df.at[index, 'curso'] = curso
             break
 counts = df['curso'].value_counts()
-print(counts)
 
+#remover colunas
 df = df.drop('Carimbo de data/hora', axis=1)
 df = df.drop('1.1 E-mail do proponente:', axis=1)
 df = df.drop('7. Coordenador(es) / CPF / Carga horária de participação:', axis=1)
@@ -25,6 +27,7 @@ df = df.drop('5. Período de execução da atividade:', axis=1)
 df = df.drop('15. Descreva os principais fatos ocorridos antes e durante a atividade:', axis=1)
 df = df.drop('16. Elabore uma interpretação crítica desta atividade:', axis=1)
 
+#renomear colunas
 df = df.rename(columns={'Categoria do projeto': 'areaTematica'})
 df = df.rename(columns={'3. Modalidade:': 'modalidade'})
 df = df.rename(columns={'1. Título da atividade:': 'titulo'})
@@ -42,6 +45,7 @@ df = df.rename(columns={'Suporte Financeiro': 'suporteFinanceiro'})
 df['usuarioId'] = 0
 df = df.rename(columns={'2. Campus / Unidade / Curso: *': 'campusId'})
 
+#reposicionando colunas
 colunas = df.columns.tolist()
 colunas.insert(0, colunas.pop(colunas.index('areaTematica')))
 colunas.insert(1, colunas.pop(colunas.index('modalidade')))
@@ -61,15 +65,16 @@ colunas.insert(14, colunas.pop(colunas.index('usuarioId')))
 colunas.insert(15, colunas.pop(colunas.index('campusId')))
 df = df[colunas]
 
-#areaTematica
+#refatorando coluna areaTematica
 df['areaTematica'] = df['areaTematica'].replace({'Pesquisa': 'PESQUISA', 'Extensão': 'EXTENSAO'})
 
-#modalidade
+#refatorando coluna modalidade
 df['modalidade'] = df['modalidade'].str.split('/').str[0]
 df['modalidade'] = df['modalidade'].str.strip()
 df['modalidade'] = df['modalidade'].str.upper()
 df = df.drop(df[df['modalidade'] == 'PRESTAÇÃO DE SERVIÇOS'].index)
 
+#salvando arquivo
 df.to_excel('novo_arquivo.xlsx', index=False)
 
 
