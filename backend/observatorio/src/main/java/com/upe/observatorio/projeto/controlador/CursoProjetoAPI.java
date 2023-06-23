eec870b374d0ae657b14085efce506988092198c
José Ferreira <josebazjunior09@gmail.com>
2023-06-23 13:23:53 -0300

    fix: refatorando classes relacionadas a entidade CursoProjeto

--- backend/observatorio/src/main/java/com/upe/observatorio/projeto/controlador/CursoProjetoAPI.java
+++ backend/observatorio/src/main/java/com/upe/observatorio/projeto/controlador/CursoProjetoAPI.java
@@ -1,80 +1,70 @@
 package com.upe.observatorio.projeto.controlador;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.validation.Valid;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.upe.observatorio.projeto.controlador.modelo.CursoProjetoRepresentacao;
 import com.upe.observatorio.projeto.dominio.CursoProjeto;
 import com.upe.observatorio.projeto.dominio.dto.CursoProjetoDTO;
 import com.upe.observatorio.projeto.servico.CursoProjetoServico;
 import com.upe.observatorio.utils.ObservatorioExcecao;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/curso-projeto")
 @CrossOrigin
+@RequiredArgsConstructor
 public class CursoProjetoAPI {
 
-	@Autowired
-	private CursoProjetoServico servico;
+    private final CursoProjetoServico servico;
 
-	@GetMapping
-	public ResponseEntity<List<CursoProjetoRepresentacao>> listarCursoProjetos() {
-		return ResponseEntity.ok(servico.listarCursoProjetos().stream().map(cursoProjeto -> convert(cursoProjeto))
-				.collect(Collectors.toList()));
-	}
+    @GetMapping
+    public ResponseEntity<List<CursoProjetoRepresentacao>> listarCursoProjetos() {
+        return ResponseEntity.ok(servico.listarCursoProjetos().stream().map(CursoProjetoRepresentacao::new)
+                .collect(Collectors.toList()));
+    }
 
-	@GetMapping("/{id}")
-	public ResponseEntity<CursoProjetoRepresentacao> buscarCursoProjetoPorId(@PathVariable("id") Long id) {
-		CursoProjetoRepresentacao resultado = convert(servico.buscarCursoProjetoPorId(id).get());
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarCursoProjetoPorId(@PathVariable("id") Long id) {
+        ResponseEntity<?> resposta;
+        try {
+            CursoProjeto cursoProjeto = servico.buscarCursoProjetoPorId(id).orElseThrow();
+            CursoProjetoRepresentacao resultado = new CursoProjetoRepresentacao(cursoProjeto);
+            resposta = ResponseEntity.ok(resultado);
+        } catch (ObservatorioExcecao e) {
+            resposta = ResponseEntity.badRequest().body(e.getMessage());
+        }
 
-		return ResponseEntity.status(HttpStatus.OK).body(resultado);
-	}
+        return resposta;
+    }
 
-	@PostMapping
-	public ResponseEntity<?> adicionarCursoProjeto(@RequestBody @Valid CursoProjetoDTO cursoProjeto) {
-		try {
-			ModelMapper modelMapper = new ModelMapper();
-			CursoProjetoRepresentacao resultado = modelMapper.map(servico.adicionarCursoProjeto(cursoProjeto),
-					CursoProjetoRepresentacao.class);
+    @PostMapping
+    public ResponseEntity<?> adicionarCursoProjeto(@RequestBody @Valid CursoProjetoDTO cursoProjeto) {
+        try {
+            ModelMapper modelMapper = new ModelMapper();
+            CursoProjetoRepresentacao resultado = modelMapper.map(servico.adicionarCursoProjeto(cursoProjeto),
+                    CursoProjetoRepresentacao.class);
 
-			return ResponseEntity.status(HttpStatus.CREATED).body(resultado);
+            return ResponseEntity.status(HttpStatus.CREATED).body(resultado);
 
-		} catch (ObservatorioExcecao e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
-	}
+        } catch (ObservatorioExcecao e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 
-	@DeleteMapping("/{id}")
-	public ResponseEntity<?> removerCursoProjeto(@PathVariable("id") Long id) {
-		try {
-			servico.removerCursoProjeto(id);
-		} catch (ObservatorioExcecao e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> removerCursoProjeto(@PathVariable("id") Long id) {
+        try {
+            servico.removerCursoProjeto(id);
+        } catch (ObservatorioExcecao e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
 
-		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-	}
-
-	private CursoProjetoRepresentacao convert(CursoProjeto entidade) {
-		ModelMapper modelMapper = new ModelMapper();
-		CursoProjetoRepresentacao resultado = modelMapper.map(entidade, CursoProjetoRepresentacao.class);
-
-		return resultado;
-	}
-
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
--- backend/observatorio/src/main/java/com/upe/observatorio/projeto/servico/CursoProjetoServico.java
+++ backend/observatorio/src/main/java/com/upe/observatorio/projeto/servico/CursoProjetoServico.java
@@ -1,87 +1,85 @@
 package com.upe.observatorio.projeto.servico;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.upe.observatorio.projeto.dominio.Curso;
 import com.upe.observatorio.projeto.dominio.CursoProjeto;
 import com.upe.observatorio.projeto.dominio.Projeto;
 import com.upe.observatorio.projeto.dominio.dto.CursoProjetoDTO;
 import com.upe.observatorio.projeto.repositorio.CursoProjetoRepositorio;
 import com.upe.observatorio.utils.ObservatorioExcecao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class CursoProjetoServico {
-	
-	@Autowired
-	private CursoProjetoRepositorio repositorio;
-	
-	@Autowired
-	private CursoServico cursoServico;
-	
-	@Autowired
-	private ProjetoServico projetoServico;
-	
-	public List<CursoProjeto> listarCursoProjetos() {
-		return repositorio.findAll();
-	}
 
-	public Optional<CursoProjeto> buscarCursoProjetoPorId(Long id) {
-		return repositorio.findById(id);
-	}
+    private final CursoProjetoRepositorio repositorio;
+    private final CursoServico cursoServico;
+    private final ProjetoServico projetoServico;
 
-	public CursoProjeto adicionarCursoProjeto(CursoProjetoDTO cursoProjeto) throws ObservatorioExcecao {
+    public List<CursoProjeto> listarCursoProjetos() {
+        return repositorio.findAll();
+    }
 
-		Optional<Curso> cursoExistente = cursoServico.buscarCursoPorId(cursoProjeto.getCursoId());
-		if (cursoExistente.isEmpty()) {
-			throw new ObservatorioExcecao("O curso informado não existe: " + cursoExistente.get().getId());
-		}
-		
-		Optional<Projeto> projetoExistente = projetoServico.buscarProjetoPorId(cursoProjeto.getProjetoId());
-		if (projetoExistente.isEmpty()) {
-			throw new ObservatorioExcecao("O projeto informado não existe");
-		}
-		
-		Optional<CursoProjeto> cursoProjetoExistente = repositorio.findByCursoAndProjeto(cursoExistente.get(), projetoExistente.get());
-		if (cursoProjetoExistente.isPresent()) {
-			throw new ObservatorioExcecao("Já existe um relacionamento criado entre o curso e o projeto informado");
-		}
-		
-		CursoProjeto cursoProjetoSalvar = new CursoProjeto();
-		cursoProjetoSalvar.setCurso(cursoExistente.get());
-		cursoProjetoSalvar.setProjeto(projetoExistente.get());
-		
-		return repositorio.save(cursoProjetoSalvar);
-	}
+    public Optional<CursoProjeto> buscarCursoProjetoPorId(Long id) throws ObservatorioExcecao {
+        if (repositorio.findById(id).isEmpty()) {
+            throw new ObservatorioExcecao("Não existe um campusCurso associado a este id: " + id);
+        }
+        return repositorio.findById(id);
+    }
 
-	public void removerCursoProjeto(Long id) throws ObservatorioExcecao {
-		if (repositorio.findById(id).isEmpty()) {
-			throw new ObservatorioExcecao("Não existe um relacionamento entre curso e projeto associado a este id");
-		}
-		
-		repositorio.deleteById(id);	
-	}
-	
-	public HashMap<String, Integer> obterQuantidadeDeProjetosPorCurso() throws ObservatorioExcecao {
-		HashMap<String, Integer> resultado = new HashMap<>();
-		List<CursoProjeto> cursoProjetos = repositorio.findAll();
-		
-		for (CursoProjeto cursoProjeto : cursoProjetos) {
-			Curso curso = cursoServico.buscarCursoPorId(cursoProjeto.getCurso().getId()).get();
-			
-			if (resultado.containsKey(curso.getNome())) {
-				Integer qtdProjetos = resultado.get(curso.getNome());
-				resultado.put(curso.getNome(), qtdProjetos + 1);
-			} else {
-				resultado.put(curso.getNome(), 1);
-			}
-		}
-		
-		return resultado;
-	}
+    public CursoProjeto adicionarCursoProjeto(CursoProjetoDTO cursoProjeto) throws ObservatorioExcecao {
+
+        Optional<Curso> cursoExistente = cursoServico.buscarCursoPorId(cursoProjeto.getCursoId());
+        if (cursoExistente.isEmpty()) {
+            throw new ObservatorioExcecao("O curso informado não existe: ");
+        }
+
+        Optional<Projeto> projetoExistente = projetoServico.buscarProjetoPorId(cursoProjeto.getProjetoId());
+        if (projetoExistente.isEmpty()) {
+            throw new ObservatorioExcecao("O projeto informado não existe");
+        }
+
+        Optional<CursoProjeto> cursoProjetoExistente = repositorio.findByCursoAndProjeto(cursoExistente.get(), projetoExistente.get());
+        if (cursoProjetoExistente.isPresent()) {
+            throw new ObservatorioExcecao("Já existe um relacionamento criado entre o curso e o projeto informado");
+        }
+
+        CursoProjeto cursoProjetoSalvar = new CursoProjeto();
+        cursoProjetoSalvar.setCurso(cursoExistente.get());
+        cursoProjetoSalvar.setProjeto(projetoExistente.get());
+
+        return repositorio.save(cursoProjetoSalvar);
+    }
+
+    public void removerCursoProjeto(Long id) throws ObservatorioExcecao {
+        if (repositorio.findById(id).isEmpty()) {
+            throw new ObservatorioExcecao("Não existe um relacionamento entre curso e projeto associado a este id");
+        }
+
+        repositorio.deleteById(id);
+    }
+
+    public HashMap<String, Integer> obterQuantidadeDeProjetosPorCurso() throws ObservatorioExcecao {
+        HashMap<String, Integer> resultado = new HashMap<>();
+        List<CursoProjeto> cursoProjetos = repositorio.findAll();
+
+        for (CursoProjeto cursoProjeto : cursoProjetos) {
+            Curso curso = cursoServico.buscarCursoPorId(cursoProjeto.getCurso().getId()).orElseThrow();
+
+            if (resultado.containsKey(curso.getNome())) {
+                Integer qtdProjetos = resultado.get(curso.getNome());
+                resultado.put(curso.getNome(), qtdProjetos + 1);
+            } else {
+                resultado.put(curso.getNome(), 1);
+            }
+        }
+
+        return resultado;
+    }
 
 }
