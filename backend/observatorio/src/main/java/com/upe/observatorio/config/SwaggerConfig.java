0cd5801a6a41949f422b262fb65caf41bf224eed
Jose Ferreira <josebazjunior09@gmail.com>
2023-10-13 14:55:37 -0300

    [fix]: add order by methods on swagger

--- backend/observatorio/src/main/java/com/upe/observatorio/config/SwaggerConfig.java
+++ backend/observatorio/src/main/java/com/upe/observatorio/config/SwaggerConfig.java
@@ -1,23 +1,20 @@
 package com.upe.observatorio.config;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.OperationsSorter;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -26,13 +23,18 @@ public class SwaggerConfig {
 	public Docket swagger() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.apiInfo(apiInfo())
-			    .securityContexts(Arrays.asList(securityContext()))
-			    .securitySchemes(Arrays.asList(apiKey()))
+			    .securityContexts(Collections.singletonList(securityContext()))
+			    .securitySchemes(List.of(apiKey()))
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.upe.observatorio"))
 				.paths(PathSelectors.any()).build();
 	}
-	
+
+	@Bean
+	UiConfiguration uiConfig() {
+		return UiConfigurationBuilder.builder().operationsSorter(OperationsSorter.METHOD).build();
+	}
+
 	private ApiInfo apiInfo() {
 	    return new ApiInfo(
 	      "API-OPUPE",
@@ -57,7 +59,7 @@ public class SwaggerConfig {
 	    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything"); 
 	    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1]; 
 	    authorizationScopes[0] = authorizationScope; 
-	    return Arrays.asList(new SecurityReference("JWT", authorizationScopes)); 
+	    return List.of(new SecurityReference("JWT", authorizationScopes));
 	}
 	
 }
