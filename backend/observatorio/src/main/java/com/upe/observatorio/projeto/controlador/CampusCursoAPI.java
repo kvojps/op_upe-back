debf65f3fdf7014d4190bb111906275408dd19c1
Jose Ferreira <josebazjunior09@gmail.com>
2023-10-13 01:39:23 -0300

    [fix]: refactor campuscourse service methods

--- backend/observatorio/src/main/java/com/upe/observatorio/projeto/controlador/CampusCursoAPI.java
+++ backend/observatorio/src/main/java/com/upe/observatorio/projeto/controlador/CampusCursoAPI.java
@@ -2,12 +2,15 @@ package com.upe.observatorio.projeto.controlador;
 
 import com.upe.observatorio.projeto.controlador.modelo.CampusCursoRepresentacao;
 import com.upe.observatorio.projeto.dominio.CampusCurso;
+import com.upe.observatorio.projeto.dominio.dto.CampusCursoDTO;
 import com.upe.observatorio.projeto.servico.CampusCursoServico;
 import com.upe.observatorio.utils.ObservatorioExcecao;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,46 +22,26 @@ public class CampusCursoAPI {
 
     private final CampusCursoServico servico;
 
-    @GetMapping
-    public ResponseEntity<List<CampusCursoRepresentacao>> listarCampusCurso() {
-        return ResponseEntity
-                .ok(servico.listarCampusCurso().stream().map(CampusCursoRepresentacao::new).collect(Collectors.toList()));
+    @PostMapping
+    public ResponseEntity<?> adicionarCampusCurso(@RequestBody @Valid CampusCursoDTO campusCurso) {
+        try {
+            CampusCursoRepresentacao resultado = new CampusCursoRepresentacao(servico.adicionarCampusCurso(campusCurso));
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(resultado);
+
+        } catch (ObservatorioExcecao e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> buscarCampusCursoPorId(@PathVariable("id") Long id) {
-		ResponseEntity<?> resposta;
-		try {
-			CampusCurso campus = servico.buscarCampusCursoPorId(id).orElseThrow();
-			CampusCursoRepresentacao resultado = new CampusCursoRepresentacao(campus);
-			resposta = ResponseEntity.ok(resultado);
-		} catch (ObservatorioExcecao e) {
-			resposta = ResponseEntity.badRequest().body(e.getMessage());
-		}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> removerCampusCurso(@PathVariable("id") Long id) {
+        try {
+            servico.removerCampusCurso(id);
+        } catch (ObservatorioExcecao e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
 
-		return resposta;
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
-
-//    @PostMapping
-//    public ResponseEntity<?> adicionarCampusCurso(@RequestBody @Valid CampusCursoDTO campusCurso) {
-//        try {
-//            CampusCursoRepresentacao resultado = new CampusCursoRepresentacao(servico.adicionarCampusCurso(campusCurso));
-//
-//            return ResponseEntity.status(HttpStatus.CREATED).body(resultado);
-//
-//        } catch (ObservatorioExcecao e) {
-//            return ResponseEntity.badRequest().body(e.getMessage());
-//        }
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<?> removerCampusCurso(@PathVariable("id") Long id) {
-//        try {
-//            servico.removerCampusCurso(id);
-//        } catch (ObservatorioExcecao e) {
-//            return ResponseEntity.badRequest().body(e.getMessage());
-//        }
-//
-//        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-//    }
 }
--- backend/observatorio/src/main/java/com/upe/observatorio/projeto/servico/CampusCursoServico.java
+++ backend/observatorio/src/main/java/com/upe/observatorio/projeto/servico/CampusCursoServico.java
@@ -1,17 +1,13 @@
 package com.upe.observatorio.projeto.servico;
 
-import java.util.List;
-import java.util.Optional;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import com.upe.observatorio.projeto.dominio.Campus;
 import com.upe.observatorio.projeto.dominio.CampusCurso;
 import com.upe.observatorio.projeto.dominio.Curso;
 import com.upe.observatorio.projeto.dominio.dto.CampusCursoDTO;
 import com.upe.observatorio.projeto.repositorio.CampusCursoRepositorio;
 import com.upe.observatorio.utils.ObservatorioExcecao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -21,26 +17,13 @@ public class CampusCursoServico {
 	private final CampusServico campusServico;
 	private final CursoServico cursoServico;
 
-	public List<CampusCurso> listarCampusCurso() {
-		return repositorio.findAll();
-	}
-
-	public Optional<CampusCurso> buscarCampusCursoPorId(Long id) throws ObservatorioExcecao {
-		if (repositorio.findById(id).isEmpty()) {
-			throw new ObservatorioExcecao("Não existe um CampusCurso associado a este id!");
-		}
-		return repositorio.findById(id);
-	}
-
 	public CampusCurso adicionarCampusCurso(CampusCursoDTO campusCurso) throws ObservatorioExcecao {
 		Campus campusExistente = campusServico.buscarCampusPorId(campusCurso.getCampusId());
-
 		Curso cursoExistente = cursoServico.buscarCursoPorId(campusCurso.getCursoId());
 		
-		Optional<CampusCurso> campusCursoExistente = repositorio.findByCampusAndCurso(campusExistente, cursoExistente);
-		if (campusCursoExistente.isPresent()) {
-			throw new ObservatorioExcecao("Já existe um relacionamento criado entre o campus e o curso informado");
-		}
+
+		repositorio.findByCampusAndCurso(campusExistente, cursoExistente).orElseThrow(() ->
+				new ObservatorioExcecao("Já existe um relacionamento criado entre o campuse e o curso informado"));
 		
 		CampusCurso campusCursoSalvar = new CampusCurso();
 		campusCursoSalvar.setCampus(campusExistente);
@@ -56,5 +39,4 @@ public class CampusCursoServico {
 
 		repositorio.deleteById(id);
 	}
-
 }
