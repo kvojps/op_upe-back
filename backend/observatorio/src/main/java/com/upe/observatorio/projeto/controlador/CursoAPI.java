14249b47b1dba678dad62d49b3416a0b786dd8d4
Jose Ferreira <josebazjunior09@gmail.com>
2023-12-09 19:54:23 -0300

    [feat]: adjust endpoints names on curso API

--- backend/observatorio/src/main/java/com/upe/observatorio/projeto/controlador/CursoAPI.java
+++ backend/observatorio/src/main/java/com/upe/observatorio/projeto/controlador/CursoAPI.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("api/curso")
+@RequestMapping("api/cursos")
 @CrossOrigin
 @RequiredArgsConstructor
 public class CursoAPI {
