19ade198b0aebc8e3fcbd9e5042ac15df5426d12
Jose Ferreira <josebazjunior09@gmail.com>
2023-12-09 22:20:58 -0300

    [feat]: Adição da resposta de erros da API

--- backend/observatorio/src/main/java/com/upe/observatorio/utils/GerenciadorExcecaoGlobal.java
+++ backend/observatorio/src/main/java/com/upe/observatorio/utils/GerenciadorExcecaoGlobal.java
@@ -10,26 +10,26 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GerenciadorExcecaoGlobal {
     @ExceptionHandler(ObservatorioExcecao.class)
     public ResponseEntity<Object> handleAcsException(ObservatorioExcecao ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.badRequest().body(new GlobalExceptionHandlerResponse(ex.getMessage()));
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new GlobalExceptionHandlerResponse(ex.getMessage()));
     }
 
     @ExceptionHandler(InvalidPasswordException.class)
     public ResponseEntity<Object> handleInvalidPasswordException(InvalidPasswordException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.badRequest().body(new GlobalExceptionHandlerResponse(ex.getMessage()));
     }
 
     @ExceptionHandler(RelationExistsException.class)
     public ResponseEntity<Object> handleCampusCursoRelationException(RelationExistsException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.badRequest().body(new GlobalExceptionHandlerResponse(ex.getMessage()));
     }
 
     @ExceptionHandler(ProjectResourceNotFoundException.class)
     public ResponseEntity<Object> handleProjectResourceNotFoundException(ProjectResourceNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new GlobalExceptionHandlerResponse(ex.getMessage()));
     }
 }
---
+++ backend/observatorio/src/main/java/com/upe/observatorio/utils/GlobalExceptionHandlerResponse.java
@@ -0,0 +1,14 @@
+package com.upe.observatorio.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GlobalExceptionHandlerResponse {
+    private String message;
+}
